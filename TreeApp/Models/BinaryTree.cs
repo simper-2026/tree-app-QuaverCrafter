559d294ea82d63b1e1e8ed2285910e648817b589
QuaverCrafter <77705085+QuaverCrafter@users.noreply.github.com>
2026-03-13 11:23:02 -0600

    Height method

--- TreeApp/Models/BinaryTree.cs
+++ TreeApp/Models/BinaryTree.cs
@@ -2,17 +2,52 @@ namespace models;
 
 public class BinaryTree
 {
+   public Node Root {get;}
     void Insert(int value)
     {
         
     }
     string InOrder(){}
-    int Height()
+    public int Height()
     {
+        return HeightFinder(Root);
+    }
+    private int HeightFinder(Node? current)
+    {
+        if (current == null)
+        {
+            return -1;
+        }
+        int left = HeightFinder(current.Left);
+        int right = HeightFinder(current.Right);
+        if (left>right) return left +1;
+        else return right +1;
+    }
+    public string ToMermaid()
+    {
+        int counter = 0;
+        if (Root== null)
+        {
+            return "graph TD; \n";
+        }
+        if (Root.Right == null && Root.Left == null)
+        {
+            return $"graph TD;\n"+Root.Value+"\n";
+        }
+        return $"graph TD;\n"+ToMermaid(Root, counter)+"\n";
         
     }
-    string ToMermaid()
+    private string ToMermaid(Node? current, ref int counter)
     {
-        
+        if (current == null||(current.Left == null && current.Right ==null))
+        {
+            return string.Empty;
+        }
+        string temp = string.Empty;
+        if (current.Left != null)
+        {
+            temp += node.Value+" --> "+node.Left.Value+" \n";
+            temp += ToMermaid(current.Left)
+        }
     }
 }
